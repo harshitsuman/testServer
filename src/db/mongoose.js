a278ecdc3f17283a9539689521d38aa5db97797f
Harshit Suman <harshit@matellio.com>
2021-06-29 21:51:32 +0530

    updated code

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 let host = "52.15.155.125"
 let port = 27017
 let username = "myUserDevAdmin"
-let password = "HJVjfwfvw@3242sdv"
+// const PASSWORD = encodeURIComponent('HJVjfwfvw@3242sdv');
+let password = encodeURIComponent('HJVjfwfvw@3242sdv');
+// "HJVjfwfvw@3242sdv"
 let database = "testENVDB"
 // }
 mongoose.connect(`mongodb://${username}:${password}@${host}:${port}/${database}`, { useNewUrlParser: true, useUnifiedTopology: true });
