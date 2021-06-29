247f178d655d02e875450b6ba7c7c217fe274249
Harshit Suman <harshit@matellio.com>
2021-06-29 18:29:19 +0530

    updated code

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 var app = express();
 app.use(express.json());
 
+console.log("This is the test")
+
 app.use(process.env.BASE_URL+'barcode',require('./Router/barcodeRouter'));
 var port = process.env.PORT || 3001;
 app.listen(port, console.log.bind(console,`server is up on port ${port}`));
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/testENVDB', { useNewUrlParser: true, useUnifiedTopology: true });
+let mongo_data = {
+    host: "52.15.155.125",
+    port: 27017,
+    username: "myUserDevAdmin",
+    password: "HJVjfwfvw@3242sdv",
+    database: "testENVDB"
+}
+mongoose.connect(mongo_data, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// mongoose.connect('mongodb://localhost:27017/testENVDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 var db = mongoose.connection;
 
